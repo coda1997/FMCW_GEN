c1077823aeae520d328857d2ca1387bd268c9ea6
Dadachen <overlooker1997@gmail.com>
2020-06-09 10:50:20 +0800

    fix bugs

--- fmcw_gen.py
+++ fmcw_gen.py
@@ -11,13 +11,15 @@ k = np.arange(5)
 k = k.repeat(framerate/n)
 t_prim = t - k*T 
 B = 3000 #Hz 带宽
-f_cos = np.cos(2*np.pi*f_min*t_prim+np.pi*B*(t_prim**2)/T)
+f_cos = np.cos(2*np.pi*f_min*t_prim+np.pi*B*(t_prim**2)/T)*10000 # 10000提升响度
 # f_cos is the 实数域数值大小 or wave_data
+# f_cos = wave_data = signal.chirp(t, f_min, 1, 1000, method='linear') * 10000
+# chirp波
 
 wave_data = f_cos.astype(np.short)
 
 
-wave_file = wave.open('/Users/dada/Downloads/fmcw.wav','wb')
+wave_file = wave.open('output/fmcw.wav','wb')
 wave_file.setnchannels(1)
 wave_file.setsampwidth(2)
 wave_file.setframerate(framerate)
--- wave_show.py
+++ wave_show.py
@@ -1,9 +1,9 @@
 import wave
-import pyplot as pl
+import matplotlib.pyplot as pl
 import numpy as np
 
 # 打开WAV文档
-f = wave.open('/Users/dada/Downloads/fmcw.wav', 'rb')
+f = wave.open('output/fmcw.wav', 'rb')
 
 # 读取格式信息
 # (nchannels, sampwidth, framerate, nframes, comptype, compname)
@@ -18,7 +18,7 @@ f.close()
 wave_data = np.fromstring(str_data, dtype=np.short)
 wave_data.shape = -1, 2
 wave_data = wave_data.T
-time = np.arange(0, nframes) * (1.0 / framerate)
+time = np.arange(0, nframes/2) * (1.0 / framerate)
 
 # 绘制波形
 pl.subplot(211) 
