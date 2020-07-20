d6819ce512a609487b8647cda90f1a4e67407b25
DadaChen1997 <overlooker1997@gmail.com>
2020-07-20 09:51:37 +0800

    update fmcw gen

--- fmcw_gen.py
+++ fmcw_gen.py
@@ -1,7 +1,7 @@
 import wave
 import numpy as np
 import scipy.signal as signal
-f_min = 17000
+f_min = 20000
 framerate = 44100
 t = np.arange(0,1,1.0/framerate)
 n = 5
@@ -10,7 +10,7 @@ T = 1/n #秒为单位
 k = np.arange(5)
 k = k.repeat(framerate/n)
 t_prim = t - k*T 
-B = 3000 #Hz 带宽
+B = 2000 #Hz 带宽
 f_cos = np.cos(2*np.pi*f_min*t_prim+np.pi*B*(t_prim**2)/T)*10000 # 10000提升响度
 # f_cos is the 实数域数值大小 or wave_data
 # f_cos = wave_data = signal.chirp(t, f_min, 1, 1000, method='linear') * 10000
@@ -19,11 +19,10 @@ f_cos = np.cos(2*np.pi*f_min*t_prim+np.pi*B*(t_prim**2)/T)*10000 # 10000提升�
 wave_data = f_cos.astype(np.short)
 
 
-wave_file = wave.open('output/fmcw.wav','wb')
+wave_file = wave.open('output/fmcw-high.wav','wb')
 wave_file.setnchannels(1)
 wave_file.setsampwidth(2)
 wave_file.setframerate(framerate)
 print(type(wave_file))
-wave_file.writeframes(wave_data.tostring())
+wave_file.writeframes(wave_data.tobytes())
 wave_file.close()
-
