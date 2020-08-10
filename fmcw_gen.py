5d77ebbc6a1a0c32efe855a6139301f506674d99
DadaChen1997 <overlooker1997@gmail.com>
2020-08-10 16:19:54 +0800

    support stereo ultrasound gen

--- fmcw_gen.py
+++ fmcw_gen.py
@@ -1,7 +1,7 @@
 import wave
 import numpy as np
 
-f_min = 21000
+f_min = 18000
 framerate = 44100
 t = np.arange(0, 1, 1.0 / framerate)
 n = 5
@@ -16,12 +16,20 @@ f_cos = np.cos(2 * np.pi * f_min * t_prim + np.pi * B * (t_prim ** 2) / T) * 100
 # f_cos = wave_data = signal.chirp(t, f_min, 1, 1000, method='linear') * 10000
 # chirp波
 
-wave_data = f_cos.astype(np.short)
+f_min_right = 19500
+f_right = np.cos(2 * np.pi * f_min_right * t_prim + np.pi * B * (t_prim ** 2) / T) * 10000
 
-wave_file = wave.open('output/fmcw-high.wav', 'wb')
-wave_file.setnchannels(1)
+wave_data = f_cos.astype(np.short)
+wave_data_right = f_right.astype(np.short)
+
+wave_file = wave.open('output/fmcw-low-stereo.wav', 'wb')
+wave_file.setnchannels(2)
 wave_file.setsampwidth(2)
 wave_file.setframerate(framerate)
 print(type(wave_file))
-wave_file.writeframes(wave_data.tobytes())
+
+for l, r in zip(wave_data, wave_data_right):
+    wave_file.writeframes(l.tobytes())
+    wave_file.writeframes(r.tobytes())
+
 wave_file.close()
