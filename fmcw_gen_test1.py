554c396b27ed53f046d595e5500c2d70079c1494
DadaChen1997 <overlooker1997@gmail.com>
2020-09-01 17:22:13 +0800

    update code

--- fmcw_gen_test1.py
+++ fmcw_gen_test1.py
@@ -4,7 +4,7 @@ import numpy as np
 f_min = 20000
 framerate = 48000
 t = np.arange(0, 1, 1.0 / framerate)
-n = 30
+n = 5
 T = 1 / n  # 秒为单位
 # from 0 to n-1
 k = np.arange(n)
@@ -22,18 +22,18 @@ f_cos = np.cos(2 * np.pi * f_min * t_prim + np.pi * B * (t_prim ** 2) / T) * 100
 wave_data = f_cos.astype(np.short)
 # wave_data_right = f_right.astype(np.short)
 
-wave_file = wave.open('output/fmcw_test1.wav', 'wb')
+wave_file = wave.open('output/test2.wav', 'wb')
 wave_file.setnchannels(1)
 wave_file.setsampwidth(2)
 wave_file.setframerate(framerate)
 print(type(wave_file))
 
 empty_data = np.zeros(shape=(10), dtype='short')
+wave_file.writeframes(empty_data.tobytes())
 
 for i in range(0,1):
     wave_file.writeframes(wave_data.tobytes())
 
-wave_file.writeframes(empty_data.tobytes())
 
 # for l, r in zip(wave_data, wave_data_right):
 #     wave_file.writeframes(l.tobytes())
